981236b59d21b3c271e49fe98ca09424e5b93666
VictorCarvalho16 <victor.inf2011@hotmail.com>
2020-05-01 17:49:45 -0300

    react redux typescript

---
+++ my-app/src/components/RepositoryList/index.tsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators, Dispatch } from 'redux';
+
+import { Repository } from '../../store/ducks/repositories/types';
+import { ApplicationState } from '../../store';
+
+import * as RepositoriesAction from '../../store/ducks/repositories/actions';
+import RepositoryItem from '../repositoryItem';
+
+interface StateProps {
+  repositories: Repository[]
+}
+
+interface DispatchProps {
+  loadRequest(): void
+}
+
+type Props = StateProps & DispatchProps
+
+class RepositoryList extends Component<Props> {
+  componentDidMount() {
+    const { loadRequest } = this.props;
+
+    loadRequest();
+  }
+
+  render() {
+    const { repositories } = this.props;
+
+    return (
+      <ul>
+        {repositories.map((repository) => (
+          <RepositoryItem key={repository.id} repository={repository} />
+        ))}
+      </ul>
+    );
+  }
+}
+
+const mapStateToProps = (state: ApplicationState) => ({
+  repositories: state.repositories.data,
+});
+
+const mapDispatchToProps = (dispatch: Dispatch) => bindActionCreators(RepositoriesAction, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(RepositoryList);
